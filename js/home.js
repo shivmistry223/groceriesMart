5305e4f3aab543cf3a2ef8380c50187284ac605b
Shiv Mistry <shivmistry223@gmail.com>
2024-04-01 10:44:13 -0400

    Updated Bakery and dairy at home

--- js/home.js
+++ js/home.js
@@ -106,6 +106,107 @@ const FRUITS = [
   },
 ];
 
+const DAIRY = [
+  {
+    id: "d1",
+    name: "Sealtest Homogenized",
+    img: "./component/dairy/dairy_images/MILK1.png",
+    price: 6.89,
+  },
+  {
+    id: "d2",
+    name: "Natrel Fine-filtered 2% Milk, 2 L",
+    img: "./component/dairy/dairy_images/Chocolate Milk.png",
+    price: 5.38,
+  },
+  {
+    id: "d3",
+    name: "Sealtest 1% Buttermilk, 1 L",
+    img: "./component/dairy/dairy_images/Buttermilk.png",
+    price: 2.47,
+  },
+  {
+    id: "d4",
+    name: "Gay Lea Salted Butter, 454 g, 1 lb",
+    img: "./component/dairy/dairy_images/butter.png",
+    price: 4.87,
+  },
+  {
+    id: "d5",
+    name: "Great Value Unsalted Butter, 454 g",
+    img: "./component/dairy/dairy_images/Butter Unsalted.png",
+    price: 5.48,
+  },
+  {
+    id: "d6",
+    name: "Brar's Malai Paneer, 375 g",
+    img: "./component/dairy/dairy_images/Paneer.png",
+    price: 5.97,
+  },
+  {
+    id: "d7",
+    name: "Great Value Old Cheddar Cheese, 400 g",
+    img: "./component/dairy/dairy_images/Cheese.png",
+    price: 4.44,
+  },
+  {
+    id: "d8",
+    name: "Great Value Pizza Mozzarella Cheese, 400 g",
+    img: "./component/dairy/dairy_images/Cheese2.png",
+    price: 5.98,
+  },
+];
+
+const BAKERY = [
+  {
+    id: "b1",
+    name: "All Butter Croissants, 6 croissants, 270 g",
+    img: "./component/bakery/baakery_images/Croissants.png",
+    price: 4.97,
+  },
+  {
+    id: "b2",
+    name: "Wonder White Sliced Bread, 675 g",
+    img: "./component/bakery/baakery_images/white bread.png",
+    price: 2.97,
+  },
+  {
+    id: "b3",
+    name: "Dempster’s® White Sliced Bread, 675 g",
+    img: "./component/bakery/baakery_images/D_white bread.png",
+    price: 3.57,
+  },
+  {
+    id: "b4",
+    name: "CHIPS AHOY!",
+    img: "./component/bakery/baakery_images/chips ahoy.png",
+    price: 3.48,
+  },
+  {
+    id: "b5",
+    name: "CHIPS AHOY!",
+    img: "./component/bakery/baakery_images/chips ahoy2.png",
+    price: 3.48,
+  },
+  {
+    id: "b6",
+    name: "Oreo",
+    img: "./component/bakery/baakery_images/oreo.png",
+    price: 3.48,
+  },
+  {
+    id: "b7",
+    name: "Apple Turnovers, 6 pieces, 504 g",
+    img: "./component/bakery/baakery_images/turnovers.png",
+    price: 4.97,
+  },
+  {
+    id: "b8",
+    name: "Banana Bread Chocolate Chip, 8 Pack",
+    img: "./component/bakery/baakery_images/Banana bread.png",
+    price: 4.47,
+  },
+];
 
 function showSlide(index, $slides, $carousel) {
   if (index < 0) {
@@ -145,8 +246,8 @@ function addCart(id) {
     localStorage.setItem("cart", myObjStr);
   }
 
-  alert("Item has been added to cart")
-  window.location.href = "component/cart/index.html"
+  alert("Item has been added to cart");
+  window.location.href = "component/cart/index.html";
 }
 
 function setCarouselData(ITEM = [], id) {
@@ -190,14 +291,14 @@ function setCarouselData(ITEM = [], id) {
 function setData() {
   setCarouselData(VEGETABLES, "#product1");
   setCarouselData(FRUITS, "#product2");
-  setCarouselData(VEGETABLES, "#product3");
-  setCarouselData(FRUITS, "#product4");
+  setCarouselData(DAIRY, "#product3");
+  setCarouselData(BAKERY, "#product4");
   $(".cart").click(function () {
     const productId = $(this).data("product-id");
     addCart(productId);
   });
 
-  //setSlick();
+  setSlick();
 }
 
 function setSlick() {
